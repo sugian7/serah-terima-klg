9118f5e8ddeb37f3fce12b6be64bd6c3d7a9d371
uYhxsugi <134943548+sugian7@users.noreply.github.com>
2026-02-22 21:52:07 +0700

    Refactor service worker to simplify cache deletion and fetch

--- service-worker.js
+++ service-worker.js
@@ -19,9 +19,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(k => k !== CACHE_NAME)
-          .map(k => caches.delete(k))
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
       )
     )
   );
@@ -32,8 +30,6 @@ self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then(res => res)
-      .catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
